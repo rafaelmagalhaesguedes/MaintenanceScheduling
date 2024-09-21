13016cfc8428980af704e280c6f08960a0bc39ac
rafa <rafaelmagalhaes1@live.com>
2024-09-21 12:05:19 -0300

    feat: update, delete and findById methods implemented customer controller

--- src/main/java/com/desafio/agendamentos/controllers/CustomerController.java
+++ src/main/java/com/desafio/agendamentos/controllers/CustomerController.java
@@ -2,8 +2,10 @@ package com.desafio.agendamentos.controllers;
 
 import com.desafio.agendamentos.controllers.dtos.CustomerRequest;
 import com.desafio.agendamentos.controllers.dtos.CustomerResponse;
+import com.desafio.agendamentos.entities.Customer;
 import com.desafio.agendamentos.services.CustomerService;
 import com.desafio.agendamentos.services.exceptions.CustomerExistsException;
+import com.desafio.agendamentos.services.exceptions.CustomerNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +27,27 @@ public class CustomerController {
     public CustomerResponse create(@RequestBody @Valid CustomerRequest request) throws CustomerExistsException {
         return CustomerResponse.fromEntity(
                 customerService.create(request.toEntity()));
+    }
+
+    @GetMapping("/{customerId}")
+    public CustomerResponse findById(@PathVariable Long customerId) throws CustomerNotFoundException {
+        return CustomerResponse.fromEntity(
+                customerService.findById(customerId)
+        );
+    }
+
+    @PutMapping("/{customerId}")
+    public CustomerResponse update(@PathVariable Long customerId, @RequestBody @Valid CustomerRequest request) throws CustomerNotFoundException {
+        return CustomerResponse.fromEntity(
+                customerService.update(customerId, request.toEntity())
+        );
+    }
+
+    @DeleteMapping("/{customerId}")
+    public CustomerResponse delete(@PathVariable Long customerId) throws CustomerNotFoundException {
+        return CustomerResponse.fromEntity(
+                customerService.delete(customerId)
+        );
     }
 
     @GetMapping
