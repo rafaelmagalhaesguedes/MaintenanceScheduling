dfff970108d4026a2c939c3c03e39c1679e37a39
rafa <rafaelmagalhaes1@live.com>
2024-09-22 08:04:07 -0300

    feat: add javadoc crypto service

--- src/main/java/com/desafio/agendamentos/services/CryptoService.java
+++ src/main/java/com/desafio/agendamentos/services/CryptoService.java
@@ -3,25 +3,23 @@ package com.desafio.agendamentos.services;
 import org.jasypt.util.text.StrongTextEncryptor;
 
 /**
- * Crypto Service
- * *
- * Classe para Encriptação e Decriptação  dos dados.
- * Nesta classe utilizamos a biblioteca JASYPT para
- * simplificar o processo.
- **/
+ * Serviço para criptografar e descriptografar textos.
+ * Para facilitar o processo utilizamos a biblioteca JASYPT.
+ */
 public class CryptoService {
     private static final StrongTextEncryptor encryptor;
     private static final String SECRET_KEY = "mySecretKeyToEncrypt";
 
-    // Bloco estático para inicializar o encryptor com a chave secreta
+    // Inicializa o encryptor com a chave secreta
     static {
         encryptor = new StrongTextEncryptor();
         encryptor.setPassword(SECRET_KEY);
     }
 
     /**
-     * Método para criptografar um texto bruto.
-     * @param rawText Texto a ser criptografado.
+     * Criptografa um texto em formato original.
+     *
+     * @param rawText Texto em formato original a ser criptografado.
      * @return Texto criptografado.
      */
     public static String encrypt(String rawText) {
@@ -29,9 +27,10 @@ public class CryptoService {
     }
 
     /**
-     * Método para descriptografar um texto criptografado.
+     * Descriptografa um texto criptografado.
+     *
      * @param encryptedText Texto criptografado a ser descriptografado.
-     * @return Texto descriptografado.
+     * @return Texto em formato original.
      */
     public static String decrypt(String encryptedText) {
         return encryptor.decrypt(encryptedText);
