ba1b5c8ce41b6c3ffe0412f45eaab6b815ac06ba
rafa <rafaelmagalhaes1@live.com>
2024-09-22 08:05:43 -0300

    feat: scheduling date validation created

---
+++ src/main/java/com/desafio/agendamentos/services/exceptions/SchedulingDateException.java
@@ -0,0 +1,7 @@
+package com.desafio.agendamentos.services.exceptions;
+
+public class SchedulingDateException extends RuntimeException {
+    public SchedulingDateException(String message) {
+        super(message);
+    }
+}
