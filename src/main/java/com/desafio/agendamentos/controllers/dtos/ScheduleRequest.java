a6298a8764778ad60b34c8ead74e432d6cd35b2e
rafa <rafaelmagalhaes1@live.com>
2024-09-30 07:27:13 -0300

    refact: applying builder to customer schedule create

--- src/main/java/com/desafio/agendamentos/controllers/dtos/ScheduleRequest.java
+++ src/main/java/com/desafio/agendamentos/controllers/dtos/ScheduleRequest.java
@@ -1,11 +1,23 @@
 package com.desafio.agendamentos.controllers.dtos;
 
 import com.desafio.agendamentos.entities.Schedule;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record ScheduleRequest(LocalDateTime dateSchedule, String descriptionService) {
+public record ScheduleRequest(
+        @NotBlank(message = "Date service is mandatory")
+        LocalDateTime dateSchedule,
+
+        @NotBlank(message = "Description service is mandatory")
+        @Size(min = 2, max = 500, message = "Description must be between 2 and 500 characters")
+        String descriptionService
+) {
     public Schedule toEntity() {
-        return new Schedule(dateSchedule, descriptionService);
+        return Schedule.builder()
+                .dateSchedule(dateSchedule)
+                .descriptionService(descriptionService)
+                .build();
     }
 }
