3dca5ee63dd3c6bd00698034d95ed44e5813b142
rafa <rafaelmagalhaes1@live.com>
2024-09-30 09:01:33 -0300

    refact: apply builder pattern on VehicleMocks

--- src/test/java/com/desafio/agendamentos/mocks/VehicleMocks.java
+++ src/test/java/com/desafio/agendamentos/mocks/VehicleMocks.java
@@ -3,20 +3,21 @@ package com.desafio.agendamentos.mocks;
 import com.desafio.agendamentos.entities.Vehicle;
 
 public class VehicleMocks {
-    public static final Vehicle VEHICLE = new Vehicle(
-            1L,
-            "Model 1",
-            "ABC-1234",
-            "Brand 1",
-            2020,
-            CustomerMocks.CUSTOMER
-    );
+    public static final Vehicle VEHICLE = Vehicle.builder()
+            .id(1L)
+            .model("Model 1")
+            .licensePlate("ABC-1234")
+            .manufacturer("Brand 1")
+            .year(2020)
+            .customer(CustomerMocks.CUSTOMER)
+            .build();
 
-    public static Vehicle VEHICLE_UPDATED = new Vehicle(
-            1L,
-            "GHI-9101",
-            "Fiat 147",
-            "Fiat",
-            1977
-    );
+    public static final Vehicle VEHICLE_UPDATED = Vehicle.builder()
+            .id(1L)
+            .model("Fiat 147")
+            .licensePlate("GHI-9101")
+            .manufacturer("Fiat")
+            .year(1977)
+            .customer(CustomerMocks.CUSTOMER) // Assuming the customer remains the same
+            .build();
 }
